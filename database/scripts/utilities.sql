fcfc4df629085bdf8761007378fc547ea7532215
Miguel Pazo <miguel.ps19@gmail.com>
2016-12-10 08:47:04 -0500

    backups

--- database/scripts/utilities.sql
+++ database/scripts/utilities.sql
@@ -1,49 +1,32 @@
 /*************************************************************/
 /**********************RESETEAR CATEGORIA*********************/
-SELECT * FROM categories WHERE tournament_id = 3
-AND description LIKE '%MULET%';
+SELECT * FROM categories WHERE tournament_id = 4
+AND description LIKE '%PREMIO PISOS PARA CAPONES%';
 
-DELETE FROM stages WHERE category_id = 122;
+DELETE FROM stages WHERE category_id = 211;
 
--- CON SELECCION => 'assistance'
+-- CON SELECCION => null
 -- SIN SELECCION => 'selection'
-UPDATE categories SET STATUS = 'active', actual_stage = 'assistance'
-WHERE id = 122;
+UPDATE categories SET STATUS = 'inactive', actual_stage = NULL
+WHERE id = 211;
 
-UPDATE category_users SET actual_stage = 'assistance' 
-WHERE category_id = 122;
+UPDATE category_users SET actual_stage = NULL 
+WHERE category_id = 211;
 
 -- CON SELECCION => POSITION = null
 -- SIN SELECCION => POSITION = 0 
 UPDATE competitors SET points = NULL, STATUS = IF(STATUS = 'limp','present', STATUS), POSITION = NULL
-WHERE category_id = 122;
+WHERE category_id = 211;
+
+DELETE FROM competitors WHERE category_id = 211;
+DELETE FROM stages WHERE category_id = 211;
 
 
 /*************************************************************/
 /******************VER ESTADO DE CADA JURADO******************/
-SELECT * FROM category_users WHERE category_id = 123
+SELECT a.dirimente, a.actual_stage, b.names, b.lastname
+FROM category_users a
+INNER JOIN users b ON b.id = a.user_id
+WHERE category_id = 206
 
 
-SELECT * FROM agents WHERE NAMES LIKE 'ARTURO GILMER%' 2146
-4708
-SELECT * FROM animal_agent WHERE animal_id = 4708 OWNER
-SELECT * FROM catalogs WHERE category_id = 122
-
-INSERT INTO animal_agent (animal_id,agent_id,TYPE ) VALUES (4708,2146,'owner')
-
-SELECT * FROM competitors WHERE category_id=124
-SELECT * FROM categories WHERE id=122
-
-
-122
-catalog
-1 - 5
-2 - 6
-3- 7
-4- 8
-5 - 9
-
-
- UPDATE categories SET STATUS ='inactive' WHERE id=122
- 
- 
--- database/scripts/views_host.sql
+++ database/scripts/views_host.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE VIEW animal_report_short AS
+CREATE VIEW animal_report_short AS
 SELECT a.id, a.name, c.prefix, c.names
 FROM animals a
 LEFT JOIN animal_agent b ON b.animal_id = a.id AND b.type = 'breeder'
 INNER JOIN agents c ON c.id = b.agent_id
 WHERE a.deleted_at IS NULL;
 
-CREATE OR REPLACE VIEW animal_report AS
+CREATE VIEW animal_report AS
 SELECT a.id, a.code, a.name, DATE_FORMAT(a.birthdate, '%d-%m-%Y') AS birthdate, c.prefix, 
 TRIM(IF(c.names IS NULL, '', c.names)) AS breeder, TRIM(IF(e.names IS NULL, '', e.names)) AS 'owner',
 mom.prefix AS mom_prefix, mom.name AS mom_name, dad.prefix AS dad_prefix, dad.name AS dad_name
@@ -18,12 +18,12 @@ LEFT JOIN animal_report_short mom ON mom.id = a.mom
 LEFT JOIN animal_report_short dad ON dad.id = a.dad
 WHERE a.deleted_at IS NULL;
 
-CREATE OR REPLACE VIEW animal_tournament_short AS
+CREATE VIEW animal_tournament_short AS
 SELECT tournament_id, animal_id, COUNT(*) AS total_categories
 FROM catalogs
 GROUP BY animal_id, tournament_id;
 
-CREATE OR REPLACE VIEW animal_tournament AS
+CREATE VIEW animal_tournament AS
 SELECT a.*, d.prefix, b.name, b.code, DATE_FORMAT(b.birthdate, '%d-%m-%Y') AS birthdate, d.names AS 'breeder', f.names AS 'owner'
 FROM animal_tournament_short a
 INNER JOIN animals b ON b.id = a.animal_id
@@ -33,7 +33,7 @@ LEFT JOIN animal_agent e ON e.animal_id = b.id AND e.type = 'owner'
 LEFT JOIN agents f ON f.id = e.agent_id
 WHERE b.deleted_at IS NULL;
 
-CREATE OR REPLACE VIEW catalog_report AS
+CREATE VIEW catalog_report AS
 SELECT c.tournament_id AS tournament_id, c.id AS category_id, c.mode, c.order, c.description AS description, ca.group, ca.number, ca.animal_id, 
 ar.prefix, ar.name, ar.code, ar.birthdate, ar.dad_prefix, ar.dad_name, ar.mom_prefix, ar.mom_name, ar.breeder, ar.owner
 FROM categories c
@@ -43,7 +43,7 @@ LEFT JOIN animals a ON a.id = ca.animal_id
 WHERE c.status <> 'deleted' AND (ca.outsider = 0 OR ca.outsider IS NULL)
 ORDER BY c.tournament_id, c.order, ca.group, a.birthdate DESC;
 
-CREATE OR REPLACE VIEW audit_user AS
+CREATE VIEW audit_user AS
 SELECT b.id, b.user, b.names, b.lastname, a.ip, MAX(created_at) AS created_at
 FROM audits a
 INNER JOIN users b ON b.id = a.user_id
