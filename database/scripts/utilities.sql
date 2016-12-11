61787caf7bf1117504e4a9147606f69212292fe9
Miguel Pazo <miguel.ps19@gmail.com>
2016-12-11 11:21:54 -0500

    last tournament

--- database/scripts/utilities.sql
+++ database/scripts/utilities.sql
@@ -1,25 +1,25 @@
 /*************************************************************/
 /**********************RESETEAR CATEGORIA*********************/
 SELECT * FROM categories WHERE tournament_id = 4
-AND description LIKE '%PREMIO PISOS PARA CAPONES%';
+AND description LIKE '%PREMIO AFICION%';
 
-DELETE FROM stages WHERE category_id = 211;
+DELETE FROM stages WHERE category_id = 229;
 
 -- CON SELECCION => null
 -- SIN SELECCION => 'selection'
 UPDATE categories SET STATUS = 'inactive', actual_stage = NULL
-WHERE id = 211;
+WHERE id = 229;
 
 UPDATE category_users SET actual_stage = NULL 
-WHERE category_id = 211;
+WHERE category_id = 229;
 
 -- CON SELECCION => POSITION = null
 -- SIN SELECCION => POSITION = 0 
 UPDATE competitors SET points = NULL, STATUS = IF(STATUS = 'limp','present', STATUS), POSITION = NULL
-WHERE category_id = 211;
+WHERE category_id = 229;
 
-DELETE FROM competitors WHERE category_id = 211;
-DELETE FROM stages WHERE category_id = 211;
+DELETE FROM competitors WHERE category_id = 229;
+DELETE FROM stages WHERE category_id = 229;
 
 
 /*************************************************************/
@@ -27,6 +27,6 @@ DELETE FROM stages WHERE category_id = 211;
 SELECT a.dirimente, a.actual_stage, b.names, b.lastname
 FROM category_users a
 INNER JOIN users b ON b.id = a.user_id
-WHERE category_id = 206
+WHERE category_id = 231
 
 
